3607b10bad6d380cc9011fc8258ffc1ce471c9bf
Snapper Morgan <snappermorgan@gmail.com>
2015-02-10 12:48:55 -0500

    Update snapgen.php

--- wp-content/plugins/snapgen/snapgen.php
+++ wp-content/plugins/snapgen/snapgen.php
@@ -189,7 +189,7 @@ settings_fields('pluginPage');
 					if ($response) {
 						if(isset($response->standard_address_line1)){
 							$address = $response->standard_address_line1;
-							$rest = explode(" ",$response->standard_address_line2);
+							$rest = explode(" ",$response->standard_location);
 							$city = $rest[0];
 							$state = $rest[1];
 							$zip = $rest[2];
