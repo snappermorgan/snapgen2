46ca10370cdf5e70f8cb0ac81481a64814d21cc9
Clyde Morgan <snappermorgan@gmail.com>
2015-02-23 13:12:15 -0500

    putting theme assets inlace

--- wp-content/themes/snapgen/Gruntfile.js
+++ wp-content/themes/snapgen/Gruntfile.js
@@ -12,19 +12,34 @@ module.exports = function(grunt) {
     web_path: web_path
   }
 
+
   grunt.initConfig({
     global_vars: global_vars,
+     sass: {
+      dist: {
+        options: {
+          outputStyle: 'nested',
+          includePaths: ['<%= global_vars.theme_scss %>']
+        },
+        files: {
+          '<%= global_vars.theme_css %>/<%= global_vars.theme_name %>.css': '<%= global_vars.theme_scss %>/<%= global_vars.theme_name %>.scss'
+        }
+      }
+    },
+
   
     copy: {
       dist: {
         files: [
-          {expand:true, cwd: 'bower_components/validationEngine/js/', src: ['jquery.validationEngine.js'],dest:'<%= global_vars.web_path %>/wp-content/themes/snapgen/js/vendor/', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/validationEngine/js/languages/', src: ['jquery.validationEngine-en.js'],dest:'<%= global_vars.web_path %>/wp-content/themes/snapgen/js/vendor/', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/validationEngine/css/', src: ['validationEngine.jquery.css'],dest:'<%= global_vars.web_path %>/wp-content/themes/snapgen/css/vendor/', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/chosen_v1.3.0/', src: ['chosen.jquery.js'],dest:'<%= global_vars.web_path %>/wp-content/themes/snapgen/js/vendor/', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/chosen_v1.3.0/', src: ['chosen.css'],dest:'<%= global_vars.web_path %>/wp-content/themes/snapgen/css/vendor/', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/chosen_v1.3.0/', src: ['*.png'],dest:'<%= global_vars.web_path %>/wp-content/themes/snapgen/css/vendor/', filter: 'isFile'},
-          {expand:true, cwd: 'bower_components/bootstrap-sass-twbs/', src: ['**'],dest:'<%= global_vars.web_path %>/wp-content/themes/snapgen/css/vendor/', filter: 'isFile'}
+          {expand:true, cwd: 'bower_components/validationEngine/js/', src: ['jquery.validationEngine.js'],dest:'<%= global_vars.user_path %>/wp-content/themes/snapgen/js/vendor/', filter: 'isFile'},
+          {expand:true, cwd: 'bower_components/validationEngine/js/languages/', src: ['jquery.validationEngine-en.js'],dest:'<%= global_vars.user_path %>/wp-content/themes/snapgen/js/vendor/', filter: 'isFile'},
+          {expand:true, cwd: 'bower_components/validationEngine/css/', src: ['validationEngine.jquery.css'],dest:'<%= global_vars.user_path %>/wp-content/themes/snapgen/css/vendor/', filter: 'isFile'},
+          {expand:true, cwd: 'bower_components/chosen_v1.3.0/', src: ['chosen.jquery.js'],dest:'<%= global_vars.user_path %>/wp-content/themes/snapgen/js/vendor/', filter: 'isFile'},
+          {expand:true, cwd: 'bower_components/chosen_v1.3.0/', src: ['chosen.css'],dest:'<%= global_vars.user_path %>/wp-content/themes/snapgen/css/vendor/', filter: 'isFile'},
+          {expand:true, cwd: 'bower_components/chosen_v1.3.0/', src: ['*.png'],dest:'<%= global_vars.user_path %>/wp-content/themes/snapgen/css/vendor/', filter: 'isFile'},
+          {expand:true, cwd: 'bower_components/bootstrap-sass-twbs/assets/stylesheets/', src: ['**/*'],dest:'<%= global_vars.user_path %>/wp-content/themes/snapgen/scss/components/bootstrap', filter: 'isFile'},
+          {expand:true, cwd: 'bower_components/bootstrap-sass-twbs/assets/fonts/', src: ['**/*'],dest:'<%= global_vars.user_path %>/wp-content/themes/snapgen/fonts/', filter: 'isFile'},
+          {expand:true, cwd: 'bower_components/bootstrap-sass-twbs/assets/stylesheets/', src: ['**/*'],dest:'<%= global_vars.user_path %>/wp-content/themes/snapgen/scss/components/bootstrap', filter: 'isFile'}
         ]
       }
     },
@@ -32,10 +47,10 @@ module.exports = function(grunt) {
     sync: {
       dist : {
         files: [
-          { cwd: '<%= global_vars.user_path %>/wp-content/plugins/', src: ['snapgen/**'], dest: '<%= global_vars.web_path %>/wp-content/plugins/', verbose: true},
-          { cwd: '<%= global_vars.user_path %>/wp-content/themes/snapgen/css/', src: ['**'], dest: '<%= global_vars.web_path %>/wp-content/snapgen/css/', verbose: true},
-          { cwd: '<%= global_vars.user_path %>/wp-content/themes/snapgen/js/', src: ['**'], dest: '<%= global_vars.web_path %>/wp-content/snapgen/js/', verbose: true},
-          { cwd: '<%= global_vars.user_path %>/wp-content/themes/snapgen/images/', src: ['**'], dest: '<%= global_vars.web_path %>/wp-content/snapgen/images/', verbose: true},
+          { cwd: '<%= global_vars.user_path %>/wp-content/plugins/snapgen/', src: ['**/*'], dest: '<%= global_vars.web_path %>/wp-content/plugins/', verbose: true},
+          { cwd: '<%= global_vars.user_path %>/wp-content/themes/snapgen/css/', src: ['**/*'], dest: '<%= global_vars.web_path %>/wp-content/snapgen/css/', verbose: true},
+          { cwd: '<%= global_vars.user_path %>/wp-content/themes/snapgen/js/', src: ['**/*'], dest: '<%= global_vars.web_path %>/wp-content/snapgen/js/', verbose: true},
+          { cwd: '<%= global_vars.user_path %>/wp-content/themes/snapgen/images/', src: ['**/*'], dest: '<%= global_vars.web_path %>/wp-content/snapgen/images/', verbose: true},
          
         ]
       }
@@ -79,7 +94,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-sync');
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['copy','sass']);
+ 
   grunt.registerTask('default', ['copy','sync','watch']);
+ 
   
 }
