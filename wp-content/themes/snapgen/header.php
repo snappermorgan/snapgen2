f7ce369efe8fbede1c3acae0bc8b64ca54a84b54
Clyde Morgan <snappermorgan@gmail.com>
2015-02-24 09:32:28 -0500

    making html framework changins

--- wp-content/themes/snapgen/header.php
+++ wp-content/themes/snapgen/header.php
@@ -25,29 +25,51 @@
 </head>
 
 <body <?php body_class(); ?>>
+<!-- Preloader -->
+            <div id="preloader"></div>
+            <!-- End preloader -->
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'snapgen' ); ?></a>
 
-	<div id="sidebar" class="sidebar">
-		<header id="masthead" class="site-header" role="banner">
-			<div class="site-branding">
+	
+<!-- Header -->
+            <header id="header-4">
+                <div class="container">
+                	<div class="row">
+                		<?php if ( has_nav_menu( 'primary' ) ) : ?>
+			<nav id="site-navigation" class="main-navigation" role="navigation">
 				<?php
-					if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php endif;
-
-					$description = get_bloginfo( 'description', 'display' );
-					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo $description; ?></p>
-					<?php endif;
+					// Primary navigation menu.
+					wp_nav_menu( array(
+						'menu_class'     => 'nav-menu',
+						'theme_location' => 'primary',
+					) );
 				?>
-				
-			</div><!-- .site-branding -->
-		</header><!-- .site-header -->
+			</nav><!-- .main-navigation -->
+		<?php endif; ?>
+
+                	</div>
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6">
+                            <div class="header-4-logo">
+                                <h1><i class="fa fa-cube"></i>Ameriquote</h1>
+                            </div>
+                        </div>
+                        <div class="col-md-8 col-sm-6 text-right">
+                            <div class="header-4-social hidden-xs">
+                                <ul class="list-inline list-unstyled">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- End header -->
 
 		<?php get_sidebar(); ?>
-	</div><!-- .sidebar -->
+	
 
 	<div id="content" class="site-content">
