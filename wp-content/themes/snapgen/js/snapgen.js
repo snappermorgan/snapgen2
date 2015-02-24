f6f2ccf0edc6b3885242a09018354cad82b4a6c0
Snapper Morgan <snappermorgan@gmail.com>
2015-02-24 19:00:51 +0000

    requires jquery

--- wp-content/themes/snapgen/js/snapgen.js
+++ wp-content/themes/snapgen/js/snapgen.js
@@ -3,45 +3,5 @@
 ----------------------------------------*/
 
 jQuery(document).ready(function($){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-        
-        $('#back-to-top').tooltip('show');
-        
-        
-        $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-$(window).load(function(){
-          $('#preloader').fadeOut('slow',function(){$(this).remove();});
-     });
-     $('.navbar-collapse').click('li', function() {
-    $('.navbar-collapse').collapse('hide');
-});
-});
-
-/*----------------------------------------
-    RESIZE FUNCTION
-----------------------------------------*/
-jQuery(window).resize(function(){
-        scrollSpyRefresh();
-        waypointsRefresh();
+    
 });
